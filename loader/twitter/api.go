8dce48a6012db56b53aa047fd35e22a62a551e74
walkure <walkure@3pf.jp>
2023-07-05 22:46:03 +0900

    random choiced auth_token use

--- loader/twitter/api.go
+++ loader/twitter/api.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"os"
 	"strings"
@@ -20,11 +21,18 @@ const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 
 // csrfToken should be [a-f0-9]{32}
 const csrfToken = "12345678901234567890123456789012"
 
-var authTokens = os.Getenv("TWITTER_AUTH_TOKENS_FROM_BROWSER")
+var authTokenList = os.Getenv("TWITTER_AUTH_TOKENS_FROM_BROWSER")
 
 func fetchFromAPI(id_str string) (*slack.Attachment, error) {
 
-	for _, authToken := range strings.Split(authTokens, ",") {
+	if authTokenList == "" {
+		return nil, errors.New("cannot get auth_token. disable apicall")
+	}
+
+	authTokens := strings.Split(authTokenList, ",")
+	rand.Shuffle(len(authTokens), func(i, j int) { authTokens[i], authTokens[j] = authTokens[j], authTokens[i] })
+
+	for _, authToken := range authTokens {
 		req, _ := http.NewRequest("GET", fmt.Sprintf("https://api.twitter.com/1.1/statuses/show/%s.json?tweet_mode=extended&cards_platform=Web-12&include_cards=1&include_reply_count=1&include_user_entities=0", id_str), nil)
 		req.Header.Set("Authorization", authHeader)
 		req.Header.Set("Cookie", fmt.Sprintf("auth_token=%s; ct0=%s; ", authToken, csrfToken))
